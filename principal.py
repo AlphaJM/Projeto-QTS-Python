4cb91f086c2701088d3c24774fee9e9210f2e5e0
AlphaJM <jmpp13@outlook.com>
2024-04-29 11:58:05 -0300

    Comecei um principio de uma função de cadastro de pessoa

--- principal.py
+++ principal.py
@@ -1,1 +1,12 @@
 # Aqui vai ser o arquivo principal do projeto
+
+def cadastrar_pessoas(nome_pessoa,idade,sexo,e_professor):
+    nome_pessoa == nome_pessoa
+    idade == idade
+    if sexo == 0:
+        sexo == "masculino"
+    elif sexo == 1:
+        sexo == "feminino"
+    else:
+        print("Não foi possivel determinar seu sexo!")
+    e_professor = e_professor
