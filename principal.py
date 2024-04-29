b8c98f1f2914f3fedcee387082bfb77c2f478be2
AlphaJM <jmpp13@outlook.com>
2024-04-29 15:41:46 -0300

    Inicio de fato do projeto, criei algumas funções básicas só para eu lembrar como funciona programar em python

--- principal.py
+++ principal.py
@@ -1,15 +1,36 @@
 # Aqui vai ser o arquivo principal do projeto
+def cadastrar_curso(nome_curso,tema_curso):
+    nome_curso == nome_curso
+    tema_curso == tema_curso
 
-def cadastrar_pessoas(nome_pessoa,idade,sexo,e_professor):
-    nome_pessoa == nome_pessoa
-    idade == idade
+def cadastrar_materia(nome_materia, quantidade_horas, curso):
+    nome_materia == nome_materia
+    quantidade_horas == quantidade_horas
+    curso == curso
+
+def cadastrar_professor(nome_professor, idade_professor, sexo):
+    nome_professor == nome_professor
+    idade_professor == idade_professor
     if sexo == 0:
         sexo == "masculino"
     elif sexo == 1:
         sexo == "feminino"
     else:
         print("Não foi possivel determinar seu sexo!")
-    e_professor = e_professor
+
+    print(nome_professor)
+    print(idade_professor)
+    print(sexo)
+
+escolha = int(input("Bem vindo ao sistema de horário, digite o número para a escolha do que fazer: \n1- Cadastrar Professor\n2- Cadastrar Materia\n3- Cadastrar Curso"))
+
+if escolha == 1:
+    nome = input("Digite o nome do Professor: ")
+    idade = input("Digite a idade do professor: ")
+    sexo = input("Digite 0 para Masculino e 1 para feminino\n")
+    cadastrar_professor(nome, idade, sexo)
+
+
 
 
 """
