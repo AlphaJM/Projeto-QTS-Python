85659d326134845aa42f5f1e2f9999a3dd288b2d
AlphaJM <jmpp13@outlook.com>
2024-05-28 20:42:48 -0300

    mudei a função main para o arquivo principal

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -28,36 +28,6 @@ tabelas = [
         'cronograma_aula'
     ]
 
-def main(informacao_db):
-    
-    try:
-        conectar_banco = psycopg2.connect(**informacao_db)
-        # Verifique a existência das tabelas
-        resultados = verificar_existencia_tabelas(conectar_banco, tabelas)
-        # Verifique se todas as tabelas existem
-        todas_existem = all(existe for _, existe in resultados)
-
-        if todas_existem:
-            print("Todas as tabelas existem.")
-            # Código para executar se todas as tabelas existem
-            # ...
-        else:
-            print("Algumas tabelas não existem:")
-            for tabela, existe in resultados:
-                if not existe:
-                    print(f" - {tabela}")
-            # Código para executar se alguma tabela não existir
-            # ...
-
-    except Exception as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
-    finally:
-        if conectar_banco:
-            conectar_banco.close()
-
-
-
-
 def verificar_existencia_tabelas(informacao_db, tabelas):
     conectar_banco = psycopg2.connect(**informacao_db)
     resultados = []
@@ -211,6 +181,3 @@ conectar_banco_de_dados(informacao_conexao_db)""" # funciona
 criar_tabelas_iniciais(informacao_conexao_db)""" #Essa porra funciona caralho
 
 """verificar_existencia_tabelas(informacao_conexao_db, tabelas)""" # funciona
-
-if __name__ == "__main__":
-    main(informacao_conexao_db)
